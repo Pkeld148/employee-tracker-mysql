af6f39750c11d8117e414751bad6ac1fc994c5e5
Paul Keldsen <Pkeld148@gmail.com>
2021-01-21 19:59:27 -0500

    view and add queries added and working

--- index.js
+++ index.js
@@ -47,39 +47,132 @@ function viewData() {
     .prompt({
       name: "viewMenu",
       type: "list",
-      message: "What would you like to view?",
+      message: "What would you like to VIEW?",
       choices: ["View All Employees", "View All Roles", "View All Departments"],
     })
     .then(function (answer) {
       switch (answer.viewMenu) {
         case "View All Employees":
-          connection.query("select * from employee;", function (err, res) {
-            console.table(res);
-            init();
-          });
+          viewAllEmployees();
           break;
         case "View All Roles":
-          connection.query("select * from role;", function (err, res) {
-            console.table(res);
-            init();
-          });
+          viewAllRoles();
           break;
         case "View All Departments":
-          connection.query("select * from department;", function (err, res) {
-            console.table(res);
-            init();
-          });
+          viewAllDepartments();
           break;
       }
     });
 }
 
-function addData() {
-  let querystring =
-    "insert into employee (first_name, last_name, role_id, manager_id) values ('Jonathan', 'Watson', 1, 1);";
-  connection.query(querystring, function (err, res) {
-    viewData();
+function viewAllEmployees() {
+  connection.query("select * from employee;", function (err, res) {
+    console.table(res);
+    init();
   });
+}
+
+function viewAllRoles() {
+  connection.query("select * from role;", function (err, res) {
+    console.table(res);
+    init();
+  });
+}
+
+function viewAllDepartments() {
+  connection.query("select * from department;", function (err, res) {
+    console.table(res);
+    init();
+  });
+}
+
+function addData() {
+  inquirer
+    .prompt({
+      name: "addMenu",
+      type: "list",
+      message: "What would you like to ADD?",
+      choices: ["Add Employee", "Add Role", "Add Department"],
+    })
+    .then(function (answer) {
+      switch (answer.addMenu) {
+        case "Add Employee":
+            inquirer
+            .prompt([
+              {
+                name: "addEmployeeFirstName",
+                type: "input",
+                message: "Enter the employee's FIRST NAME: ",
+              },
+              {
+                name: "addEmployeeLastName",
+                type: "input",
+                message: "Enter the employee's LAST NAME: ",
+              },
+              {
+                name: "assignRole",
+                type: "input",
+                message: "Please select the employee's ROLE:",
+              },
+              {
+                name: "assignManager",
+                type: "input",
+                message: "Please select the employee's MANAGER:",
+              },
+            ])
+            .then(function (answer) {
+              connection.query(
+                "insert into employee (first_name, last_name, role_id, manager_id) values (?, ?, ?, ?)",
+                [answer.addEmployeeFirstName, answer.addEmployeeLastName, answer.assignRole, answer.assignManager],
+                function (err, res) {
+                  viewAllEmployees();
+                }
+              );
+            });
+          break;
+        case "Add Role":
+          inquirer
+            .prompt([
+              {
+                name: "addRole",
+                type: "input",
+                message: "Enter the name of the Role you want to ADD: ",
+              },
+              {
+                name: "addSalary",
+                type: "input",
+                message: "What is the salary for that Role?",
+              },
+            ])
+            .then(function (answer) {
+              connection.query(
+                "insert into role (title, salary) values (?, ?)",
+                [answer.addRole, answer.addSalary],
+                function (err, res) {
+                  viewAllRoles();
+                }
+              );
+            });
+          break;
+        case "Add Department":
+          inquirer
+            .prompt({
+              name: "addDepartment",
+              type: "input",
+              message: "Enter the name of the Department you want to ADD: ",
+            })
+            .then(function (answer) {
+              connection.query(
+                "insert into department (name) values (?)",
+                answer.addDepartment,
+                function (err, res) {
+                  viewAllDepartments();
+                }
+              );
+            });
+          break;
+      }
+    });
 }
 
 function updateData() {
